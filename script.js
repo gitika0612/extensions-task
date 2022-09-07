c87a5a28a62d04ebc0d4285fcce32f16d63c0a8c
gitika0612 <bhatiagitika0612@gmail.com>
2022-09-07 20:45:49 +0530

    fix click counter

--- script.js
+++ script.js
@@ -5,33 +5,50 @@ $(document).ready(function(){
     }
     
     const button1 = $("#first");
-    let counter1 = 0;
+    const counter1El = $("#counter1");
+    let counter1 = Number(counter1El.text())
     button1.click(function (event) {
         counter1++
-        setTimeout(function() {
-            counter1 = 0;
-        }, 500);
-        if (counter1 === 3) {
+        if (counter1 % 3 === 0) {
             redirect()
-            counter1 = 0
         }
+        counter1El.text(counter1)
     });
 
     const button2 = $("#second");
-    let counter2 = 0;
+    const counter2El = $("#counter2");
+    let counter2 = Number(counter2El.text())
     button2.click(function (event) {
         counter2++
-        setTimeout(function() {
-            counter2 = 0;
-        }, 1000);
-        if (counter2 === 5) {
+        if (counter2 % 5 === 0) {
             redirect()
-            counter2 = 0
         }
+        counter2El.text(counter2)
     });
+
+    const button3 = $("#third");
+    const counter3El = $("#counter3");
+    let counter3 = Number(counter3El.text())
+    button3.click(function (event) {
+        counter3++
+        if (counter3 % 2 === 0) {
+            redirect()
+        }
+        counter3El.text(counter3)
+    });
+
+    // const button2 = $("#second");
+    // let counter2 = 0;
+    // button2.click(function (event) {
+    //     counter2++
+    //     if (counter2 === 5) {
+    //         redirect()
+    //         counter2 = 0
+    //     }
+    // });
     
-    const button3 = $("#third")
-    button3.dblclick(function() {
-        redirect()
-    })
+    // const button3 = $("#third")
+    // button3.dblclick(function() {
+    //     redirect()
+    // })
 });
